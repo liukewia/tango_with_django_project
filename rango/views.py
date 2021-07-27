2877bed49c20e1a62e377e67b7593fd566f2e928
Yi Liu <2587395l@student.gla.ac.uk>
2021-07-27 21:06:15 +0800

    chapter8: add pages

--- rango/views.py
+++ rango/views.py
@@ -28,7 +28,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
 
@@ -42,7 +42,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
@@ -82,4 +82,4 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
 
-    return render(request, 'rango/category.html', context=context_dict)
+    return render(request, 'rango/categories.html', context=context_dict)

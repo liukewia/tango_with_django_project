772af757d2ab4c358c9657b14adde7a00f3d511f
Yi Liu <2587395l@student.gla.ac.uk>
2021-07-27 17:59:19 +0800

    finished chapter 3, add about and index pages

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse(
+        "Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 
 def about(request):

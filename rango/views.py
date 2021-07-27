c51a85de6d5ac683c07755de65643369a77830e2
Yi Liu <2587395l@student.gla.ac.uk>
2021-07-27 22:05:27 +0800

    chapter 8: fix wrong direct problem

--- rango/views.py
+++ rango/views.py
@@ -82,4 +82,4 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
 
-    return render(request, 'rango/categories.html', context=context_dict)
+    return render(request, 'rango/category.html', context=context_dict)

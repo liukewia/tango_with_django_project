5e2de268f39a886e48fef722722aea9ada98a3a3
Yi Liu <2587395l@student.gla.ac.uk>
2021-07-27 17:53:18 +0800

    finished chapter 3 halfway

---
+++ rango/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Rango says hey there partner!")
+
+
+def about(request):
+    return HttpResponse(
+        "Rango says here is the about page. <a href='/rango/'>Index</a>")
